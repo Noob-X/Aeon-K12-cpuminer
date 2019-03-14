e89a8cf6cf9350a0f53083ccb9e9bc34b37dfb9b
Noob-X <ajnatakaundinya@gmail.com>
2019-03-14 20:21:16 +0800

    Fix compiler's warning.

--- miner.h
+++ miner.h
@@ -183,7 +183,7 @@ extern int scanhash_x11(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
 	uint32_t max_nonce, unsigned long *hashes_done);
 
 extern void cryptonight_hash(void* output, const void* input, size_t input_len);
-
+struct cryptonight_ctx;
 extern int scanhash_cryptonight(int thr_id, uint32_t *pdata, const uint32_t *ptarget,
 		uint32_t max_nonce, unsigned long *hashes_done, struct cryptonight_ctx *persistentctx);
 
